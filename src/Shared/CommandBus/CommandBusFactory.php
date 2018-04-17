ba057ef5383dc03c25608614e72bc104c486e61b
Piotr Macha <me@piotrmacha.pl>
2018-04-17 23:06:28 +0200

    Remove unused mapping for CommandBusFactory

--- src/Shared/CommandBus/CommandBusFactory.php
+++ src/Shared/CommandBus/CommandBusFactory.php
@@ -2,18 +2,12 @@
 
 namespace App\Shared\CommandBus;
 
-use App\Book\Command\CreateBook\CreateBookCommand;
-use App\Book\Command\CreateBook\CreateBookCommandHandler;
 use Prooph\ServiceBus\CommandBus;
 use Prooph\ServiceBus\Plugin\Plugin;
 use Prooph\ServiceBus\Plugin\Router\MessageBusRouterPlugin;
 
 class CommandBusFactory
 {
-    private static $mapping = [
-        CreateBookCommand::class => CreateBookCommandHandler::class
-    ];
-
     public static function createCommandBus(MessageBusRouterPlugin $router): CommandBus
     {
         $commandBus = new CommandBus();
