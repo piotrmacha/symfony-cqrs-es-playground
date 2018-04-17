711de78a361c0ea6c57b2032f45470a7aacc2c2b
Piotr Macha <me@piotrmacha.pl>
2018-04-17 22:46:15 +0200

    Rewrite BookListProvider to use context aware interface

--- src/Book/Application/Provider/BookListProvider.php
+++ src/Book/Application/Provider/BookListProvider.php
@@ -3,13 +3,15 @@
 namespace App\Book\Application\Provider;
 
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
 use App\Book\Application\Dto\Book;
 use Ramsey\Uuid\Uuid;
 
-class BookListProvider implements CollectionDataProviderInterface
+class BookListProvider implements RestrictedDataProviderInterface, ContextAwareCollectionDataProviderInterface
 {
-    public function supports(string $resourceClass, string $operationName = null): bool
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
         return $resourceClass === Book::class;
     }
@@ -19,9 +21,10 @@ class BookListProvider implements CollectionDataProviderInterface
      *
      * @param string $resourceClass
      * @param string|null $operationName
+     * @param array $context
      * @return array|\Traversable
      */
-    public function getCollection(string $resourceClass, string $operationName = null)
+    public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
         $x = new Book();
         $x->setId(Uuid::uuid4());
